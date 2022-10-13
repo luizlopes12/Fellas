70b4e0e78790b60aec4e4644a7110e6a2cd9bd42
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-10-13 20:52:37 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,6 @@ const switchTheme = () =>{
     document.body.classList.toggle('dark__theme')
     localStorage.setItem('theme', document.body.classList)
 }
+
+document.body.classList.toggle('light__theme')
+localStorage.setItem('theme', document.body.classList)
