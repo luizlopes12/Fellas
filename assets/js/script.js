a289d84d472f31fa4cad6ec814b9fec43be6b1a4
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-10-21 21:37:22 -0300

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,37 @@ const switchTheme = () =>{
     localStorage.setItem('theme', document.body.classList)
 
 }
+if(window.innerWidth < 800){
+    document.querySelectorAll('.projects__section .row.cards .col-4.col-sm').classList.remove('col-sm')
+}
+
+let projectsSlidePage = 1;
+const projectsSlide = (num) =>{
+    let cards = document.querySelectorAll('.projects__section .row.cards .col-sm')
+    let cardsWrapper = document.querySelector('.projects__section .row .card__wrapper')
+    if(num>1 && projectsSlidePage <= 1){
+        projectsSlidePage++
+        cards[projectsSlidePage-1].classList.toggle('active')
+        cards[projectsSlidePage].classList.toggle('active')
+        if(projectsSlidePage == 1){
+            cardsWrapper.style.transform = `initial`
+        }else{
+            cardsWrapper.style.transform = `translateX(-${(projectsSlidePage-1)*30}%)`
+        }
+    }
+    if(num<1 && projectsSlidePage >= 1){
+        projectsSlidePage--
+        cards[projectsSlidePage+1].classList.toggle('active')
+        cards[projectsSlidePage].classList.toggle('active')
+        if(projectsSlidePage == 1){
+            cardsWrapper.style.transform = `initial`
+        }else{
+            cardsWrapper.style.transform = `translateX(${(projectsSlidePage+1)*30}%)`
+        }
+    }
+    if(num==1){
+        cards[projectsSlidePage].classList.toggle('active')
+    }
+
+}
+projectsSlide(1)
